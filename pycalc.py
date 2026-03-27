b6721f19021d48d55e6493d41c6b33526260e6b9
Kodiak <asadani039@gmail.com>
2026-03-27 01:15:05 +0100

    Added the contents of my old file to it

--- pycalc.py
+++ pycalc.py
@@ -1,1 +1,9 @@
-pritn("Hello world")
+op1 = int(input("Entrez le premier opérande."))
+opr = str(input("Entrez l'opérateur. Les symboles acceptés sont +, -, *, /, //, **"))
+op2 = int(input("Entrez le deuxième opérande."))
+res = None
+
+def calcul(op1, opr, op2) -> res:
+    if opr == "+":
+        res = op1 + op2
+    return res
