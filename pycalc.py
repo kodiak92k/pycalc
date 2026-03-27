e54913d4627417ca23f4bb16d5d9c08b67567b9d
Kodiak <asadani039@gmail.com>
2026-03-27 01:17:42 +0100

    Updated formatting for easier data input

--- pycalc.py
+++ pycalc.py
@@ -1,6 +1,6 @@
-op1 = int(input("Entrez le premier opérande."))
-opr = str(input("Entrez l'opérateur. Les symboles acceptés sont +, -, *, /, //, **"))
-op2 = int(input("Entrez le deuxième opérande."))
+op1 = int(input("Entrez le premier opérande : "))
+opr = str(input("Entrez l'opérateur. Les symboles acceptés sont +, -, *, /, //, ** : "))
+op2 = int(input("Entrez le deuxième opérande : "))
 res = None
 
 def calcul(op1, opr, op2) -> res:
